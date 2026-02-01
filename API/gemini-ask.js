a1853771c76c6d4c4e330c6864b6e84f39ec5fd5
piyamijj <piyamipp@gmail.com>
2026-02-01 05:01:19 +0330

    Update gemini-ask.js

--- API/gemini-ask.js
+++ API/gemini-ask.js
@@ -44,7 +44,7 @@ export default async function handler(req) {
 
         Piyami Bey'in Mesajı: ${question}`;
 
-        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=${apiKey}`, {
+        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-latest-flash:generateContent?key=${apiKey}`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
